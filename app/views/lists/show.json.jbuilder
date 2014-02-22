93b402514130dfb2d02cf95f4bb8c9d82e2206cf
Jerri Conrad <jerriaconrad@gmail.com>
2014-02-22 12:38:35 -0800

    initial app build

---
+++ app/views/lists/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @list, :id, :item, :descritption, :qty, :store, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shopping::Application.config.secret_key_base = 'bbb92cfc6f74c5adebf26701fab109332d227a6d710785f0ecca0664356d0ccca7fd4484c36ee3aee6956d3e36f4972081496a15aedea6aca0a74a86135fdb25'
